c74d3a68179f885bf3e4d6fa9814c1ef02919d1f
Basudev <samantabasu.2001@gmail.com>
2024-02-24 23:55:39 +0530

    About section is added with html and css

---
+++ main.js
@@ -0,0 +1,13 @@
+/*=============== SHOW MENU ==================== */
+/*=============== MENU SHOW ============== */
+/*Validate if constants exists */
+/*=============== MENU HIDDEN ============== */
+/*Validate if constants exists */
+/*=============== REMOVE MENU MOBILE ==================== */
+/*=============== SCROLL SECTIONS ACTIVE LINK ==================== */
+/*=============== CHANGE BACKGROUND HEADER ==================== */
+/*=============== SHOW SCROLL UP ==================== */
+/*=============== ABOUT TABS ==================== */
+
+
+/*=============== CONTACT FORM ==================== */

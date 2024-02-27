f19f132a8899bb8627c35aeb6a7ab330db2fba3b
Basudev <samantabasu.2001@gmail.com>
2024-02-27 16:55:29 +0530

    JAVAScript added of contact section

--- main.js
+++ main.js
@@ -9,12 +9,12 @@
 /*=============== SHOW SCROLL UP ==================== */
 /*=============== ABOUT TABS ==================== */
 const tabs = document.querySelectorAll('[data-target]'),
-tabContents = document.querySelectorAll('[data-content]');
+    tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
-       
+
         tabContents.forEach((tabContent) => {
             tabContent.classList.remove('tab__active');
         });
@@ -30,3 +30,52 @@ tabs.forEach((tab) => {
 });
 
 /*=============== CONTACT FORM ==================== */
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message'),
+    errorMessage = document.getElementById('error-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    //    Check if the field has a value
+    if (
+        contactName.value === '' ||
+        contactEmail.value === '' ||
+        contactSubject.value === '' ||
+        contactMessage.value === '') {
+
+        //show message
+        errorMessage.textContent = 'Write all the input fields'
+    }
+
+    else {
+        // serviceID - templateID - #form - publickey
+        emailjs.sendForm('service_nfkooda',
+            'template_qubv8xq',
+            '#contact-form',
+            '497qMowTLPtw8FTDQ').then(() => {
+                // show message and add color, window + dot to open emoji
+                errorMessage.classList.add('color-first');
+                errorMessage.textContent = 'Message sent ✔️';
+
+                //remove after 5 seconds
+                setTimeout(() => {
+                    errorMessage.textContent = '';
+                }, 3000);
+            }, (error) => {
+                alert('OOPs! SOMETHING WENT WRONG...', error);
+            }
+            );
+
+            //clear input fields
+            contactName.value = '';
+            contactEmail.value = '';
+            contactSubject.value = '';
+            contactMessage.value = '';
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
